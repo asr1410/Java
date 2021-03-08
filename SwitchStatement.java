8505913d99295ca61d5baa5f639bd8a0972de4f9
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-03-08 14:03:48 +0530

    changes

--- SwitchStatement.java
+++ SwitchStatement.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class SwitchStatement {
     public static void main(String[] args) {
+        char method;
+        double a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        System.out.print("enter the operator :- ");
+        method = sc.next().charAt(0);
 
+        switch (method) {
+        case '+':
+            System.out.println(a + " " + method + " " + b + " = " + (a + b));
+            break;
+        case '-':
+            System.out.println(a + " " + method + " " + b + " = " + (a - b));
+            break;
+        case '*':
+            System.out.println(a + " " + method + " " + b + " = " + (a * b));
+            break;
+        case '%':
+            System.out.println(a + " " + method + " " + b + " = " + (a % b));
+            break;
+        case '/':
+            System.out.println(a + " " + method + " " + b + " = " + (a / b));
+            break;
+        default:
+            System.out.println("The operator is invalid");
+            break;
+        }
+        sc.close();
     }
 }
