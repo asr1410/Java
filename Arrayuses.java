45ffe61360d03b102c705bfdc6d0af1844f21996
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-03-09 12:42:23 +0530

    changes

--- Arrayuses.java
+++ Arrayuses.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Arrayuses {
     public static void main(String[] args) {
+        int[] marks = new int[5];
+        System.out.println(marks.length);
+        Scanner sc = new Scanner(System.in);
 
+        for (int i = 0; i < 5; i++) {
+            marks[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(marks[i]);
+        }
+        sc.close();
     }
 }
