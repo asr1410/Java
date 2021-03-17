2756a1d76152da081628e568ff2ec5c2e695e4cb
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-03-17 19:11:16 +0530

    changes

--- Pakages.java
+++ Pakages.java
@@ -1,5 +1,10 @@
+import java.util.*;
+
 public class Pakages {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("the input taken is " + n);
+        sc.close();
     }
 }
