61a9eee7ca964b3807f85a449e08572221371edd
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-03-20 17:38:39 +0530

    changes

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,6 +1,7 @@
 public class ExceptionHandling {
     public static void main(String[] args) {
         int a = 5;
-        System.out.println(a);
+        int b = 5;
+        System.out.println(a + b);
     }
 }
