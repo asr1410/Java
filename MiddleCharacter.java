863f68d33e485ee74752d3e221b6b7799979fb24
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-03-21 23:30:13 +0530

    changes

--- MiddleCharacter.java
+++ MiddleCharacter.java
@@ -7,33 +7,23 @@ public class MiddleCharacter {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string:");
-        String str = sc.nextLine();
-        char ch[] = str.toLowerCase().toCharArray();
-        int c = 0;
-        String sp = "";
-        if (str.length() < 3) {
-            System.out.println("the string should not have " + str + " is too short");
+        System.out.print("ENTER THE STRING TO FIND MIDDLE ELEMENT --> ");
+        String input = sc.next();
+        char ch[] = input.toLowerCase().toCharArray();
+        if (ch.length < 3) {
+            System.out.println("STRING IS TOO SHORT TRY WITH A NEW STRING WITH SIZE GREATER THAN 2");
         } else {
             for (int i = 0; i < ch.length; i++) {
                 if (ch[i] >= 'a' && ch[i] <= 'z') {
-                    c++;
                 } else {
-                    sp += ch[i];
+                    System.out.println("the string should only contain alphabets");
+                    System.exit(1);
                 }
             }
-            if (ch.length != c) {
-                System.out.println("This string should not have");
-                System.out.println(sp);
-            } else if (ch.length == c) {
-                System.out.println("middle characters:");
-                if (ch.length % 2 == 0) {
-                    String out = " " + ch[(ch.length / 2) - 1] + ch[(ch.length / 2)];
-                    System.out.println(out);
-                } else {
-                    String out = "" + ch[ch.length / 2];
-                    System.out.println(out);
-                }
+            if (ch.length % 2 == 0) {
+                System.out.println(ch[(ch.length / 2) - 1] + "" + ch[(ch.length) / 2]);
+            } else {
+                System.out.println(ch[ch.length / 2]);
             }
         }
         sc.close();
