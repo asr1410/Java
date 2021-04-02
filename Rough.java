cdd7523e5f195d4346a9536ad8f6d3ca75b79f3f
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-04-02 11:32:00 +0530

    changes

--- Rough.java
+++ Rough.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 /**
  * Rough
  */
 public class Rough {
 
     public static void main(String[] args) {
-        String s = "amandeep";
-        System.out.println(s.getBytes());
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int countyes = 0;
+        int countno = 0;
+        for (int i = 0; i < num; i++) {
+            String s = sc.next();
+            if (s.equals("yes") || s.equals("YES") || s.equals("Yes")) {
+                countyes++;
+            } else if (s.equals("no") || s.equals("NO") || s.equals("No")) {
+                countno++;
+            }
+        }
+        System.out.println("Yes count " + countyes);
+        System.out.println("No count " + countno);
+        sc.close();
     }
 }
