4b8c7dd758321a945d3cba7dd59984d834a0946f
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-04-13 18:09:04 +0530

    changes

--- Rough.java
+++ Rough.java
@@ -2,16 +2,8 @@
  * Rough
  */
 public class Rough {
-    int a = 32;
-
-    public static void print() {
-        int a = 30;
-    }
 
     public static void main(String[] args) {
-        int a = 20;
-        print();
-        System.out.println(a);
 
     }
 }
