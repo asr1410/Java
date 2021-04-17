12a69860233eafa3d1a8a6079f9e7f808c252ac4
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-04-17 15:21:35 +0530

    changes

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,34 +1,31 @@
-import java.util.Scanner;
-
+/**
+ * Rough
+ */
 public class BinarySearch {
-    public static boolean binarySearch(int arr[], int num) {
+    public int binarySearch(int arr[], int num) {
         int left = 0;
         int right = arr.length - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == num || arr[left] == num || arr[right] == num) {
-                return true;
-            } else if (arr[mid] > num) {
-                right = mid + 1;
-            } else {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == num) {
+                return mid;
+            } else if (arr[mid] < num) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return false;
+        return -1;
     }
 
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        System.out.println("enter the number");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.close();
-        if (binarySearch(arr, n)) {
-            System.out.println("element is present");
+        int x = 5;
+        BinarySearch s = new BinarySearch();
+        if (s.binarySearch(arr, x) == -1) {
+            System.out.println("element is not present");
         } else {
-            {
-                System.out.println("element is not present");
-            }
+            System.out.println("element is present at index " + s.binarySearch(arr, x));
         }
     }
 }
