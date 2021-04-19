7b9302aa2dc4adbc6162ba215083952bfb4f0818
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-04-19 14:06:21 +0530

    changes

--- Rough.java
+++ Rough.java
@@ -1,13 +1,9 @@
-import java.util.Scanner;
-
 /**
  * Rough
  */
 public class Rough {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
-        scan.close();
     }
 }
