067f8c5f9880c4fe9b3ddf4a12487cacd3dc907e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-04-21 13:56:28 +0530

    changes

---
+++ ArraySetWithIndexArray.java
@@ -0,0 +1,28 @@
+public class ArraySetWithIndexArray {
+    static public int[] createTargetArray(int[] nums, int[] index) {
+        int[] target = new int[nums.length];
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (index[i] == i) {
+                target[index[i]] = nums[i];
+            } else {
+                int temp;
+                for (int j = index[i]; j < i + 1; j++) {
+                    temp = target[j];
+                    target[j] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+
+        return target;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 1, 2, 3, 4 };
+        int[] index = { 0, 1, 2, 2, 1 };
+        for (int i : createTargetArray(nums, index)) {
+            System.out.println(i);
+        }
+    }
+}
